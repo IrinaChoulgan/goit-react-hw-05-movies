f429be71bfabc2bf407e011f680532a83db07f81
IrinaChoulgan <irina_choulgan@icloud.com>
2021-11-06 12:11:30 +0200

    +

--- src/components/HomePage/NotFound.js
+++ src/components/HomePage/NotFound.js
@@ -1,3 +1,4 @@
+import s from '../HomePage/HomePage.module.css';
 export default function NotFound() {
-  return <h1>Page not faund!!!</h1>;
+  return <h1 className={s.title}>Page not faund!!!</h1>;
 }
